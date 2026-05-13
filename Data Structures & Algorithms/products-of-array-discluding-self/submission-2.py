6ce2a15a13525806defa5d830fd41e0591fa4311
Joel Joby <89447078+joel909@users.noreply.github.com>
2026-05-13 16:28:47 +0530

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        l_cache = []
+        muliply_cache = 1
+        for i in range(0,len(nums)):
+            l_cache.append(muliply_cache)
+            muliply_cache = muliply_cache*nums[i]
+        final_cache = []
+        muliply_cache = 1
+        for i in range(0,len(nums)):
+            print("here")
+            index = (len(nums)-1)-i
+            final_cache.insert(0,muliply_cache*l_cache[index])
+            muliply_cache = muliply_cache*nums[index]
+        return (final_cache)
+        
+        
