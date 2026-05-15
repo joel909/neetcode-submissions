7ab81d62a5ccb03e6dd56dba4a089aca51d56cfd
Joel Joby <89447078+joel909@users.noreply.github.com>
2026-05-15 22:47:25 +0530

    Add: is-palindrome - submission-27

---
+++ Data Structures & Algorithms/is-palindrome/submission-27.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        right_end = ""
+        left_end = ""
+        right_index = 0
+        left_index = 1
+        for i in range(0,(int(len(s)/2))):
+            right_end = s[right_index]
+            left_end = s[(len(s))-left_index]
+            while right_end.isalnum() != True and right_index+1<=len(s)-1:
+                right_index += 1
+                right_end = s[right_index]
+            while left_end.isalnum() != True and left_index+1<=len(s) :
+                left_index += 1
+                left_end = s[(len(s))-left_index]
+            print(right_index,left_index)
+            print("checking right : ",right_end,"| left end is :",left_end)
+            if right_end.lower() != left_end.lower()  and right_end.isalnum() == True and right_end.isalnum() == True:
+                return False
+            right_index += 1
+            left_index += 1
+        return True
