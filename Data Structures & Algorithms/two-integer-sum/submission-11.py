c5749e4bbd96127d2a320a2a7ff96efb52c34a71
Joel Joby <89447078+joel909@users.noreply.github.com>
2026-05-21 19:50:31 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        k = set(nums)
+        indexes = list()
+        i = 0
+        for num in nums:
+            if target-num in k:
+                if target-num != num and len(k) == len(nums):
+                    indexes.append(i)
+                elif target-num == num and len(k) != len(nums):
+                    print("before removing: ", nums)                    
+                    new_list = nums.copy()
+                    new_list.remove(num)
+                    print("after removing: ", new_list)
+                    if target-num in set(new_list):
+                        indexes.append(i)
+            i+=1
+        return indexes
+        
