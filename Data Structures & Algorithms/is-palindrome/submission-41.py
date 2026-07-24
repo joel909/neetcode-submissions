c23b1187ec78cd7c565c6440bc635ab7a9509b86
Joel Joby <89447078+joel909@users.noreply.github.com>
2026-07-24 19:56:36 +0530

    Add: is-palindrome - submission-41

---
+++ Data Structures & Algorithms/is-palindrome/submission-41.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_length = len(s)
+        right_index , left_index = 0,s_length-1
+        while left_index>right_index:
+            if s[left_index].isalnum() == False:
+                left_index -= 1
+                continue
+            if s[right_index].isalnum() == False:
+                right_index += 1
+                continue
+            if s[left_index].lower() != s[right_index].lower():
+                # print("comparing ",s[left_index].lower()," and ",s[right_index].lower())
+                return False
+            left_index -= 1
+            right_index += 1
+        return True
+        
